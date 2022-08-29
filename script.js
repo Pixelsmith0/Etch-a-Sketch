2706a744f9be440ab72488e2623c64ddee3f3b9d
M <mfetterly91@gmail.com>
2022-08-29 15:24:17 -0700

    js - changed to be more dynamic-friendly. css - adjusted flex properties

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const gridContainer = document.querySelector('#grid-container');
-
-const square = document.createElement('div')
+const rowContainer = document.querySelector('#row-container');
+const row = document.createElement('div');
+const square = document.createElement('div');
 square.classList.add('square');
-// gridContainer.appendChild(square);
+row.classList.add('row');
 
-for (let i = 0; i < 256; i++) {
-    gridContainer.appendChild(square.cloneNode(true));
-    console.log(i);
-};
+let userInput = 5;
+
+function addRows(userInput) {
+    for (let i = 1; i <= (userInput * userInput); i++) {
+        if (i % userInput == 0) {
+            rowContainer.appendChild(row.cloneNode(true));
+        }
+    };
+}
+
+function addSquares(userInput) {
+    const rows = document.querySelectorAll('#row-container > *');
+    rows.forEach(row => {
+        for (let i = 1; i <= userInput; i++) {
+            row.appendChild(square.cloneNode(true));
+        }
+    });
+}
+
+addRows(userInput)
+addSquares(userInput)
