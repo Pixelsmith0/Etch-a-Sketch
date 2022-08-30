33edd7be72f3450740b96f44ca2cd260373d49d2
M <mfetterly91@gmail.com>
2022-08-30 12:11:19 -0700

    JS - Simplified addRows

--- script.js
+++ script.js
@@ -4,16 +4,14 @@ const square = document.createElement('div');
 square.classList.add('square');
 row.classList.add('row');
 
-let userInput = 25;
+let userInput = 20;
 
 function addRows(userInput) {
-    for (let i = 1; i <= (userInput * userInput); i++) {
-        if (i % userInput == 0) {
+    for (let i = 1; i <= userInput; i++) {
             rowContainer.appendChild(row.cloneNode(true));
         };
-        };
-};
-
+    };
+    
 function addSquares(userInput) {
     const rows = document.querySelectorAll('#row-container > *');
     rows.forEach(row => {
@@ -23,11 +21,14 @@ function addSquares(userInput) {
     });
 };
 
+function addHoverEffect() {
+    document.querySelectorAll('.square').forEach(square => {
+        square.addEventListener('mouseenter', (e) => {
+            square.classList.add('square-hover');
+        });
+    });
+};
+
 addRows(userInput)
 addSquares(userInput)
-
-document.querySelectorAll('.square').forEach(square => {
-    square.addEventListener('mouseenter', (e) => {
-        square.classList.add('square-hover');
-    });
-});
+addHoverEffect();
