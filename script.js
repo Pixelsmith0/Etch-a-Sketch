ea4d2b8094b8aecca380c6e6bbc8d12fbbd72350
M <mfetterly91@gmail.com>
2022-08-30 11:32:03 -0700

    JS -  Changed the hover effect to leave a trail.

--- script.js
+++ script.js
@@ -4,24 +4,30 @@ const square = document.createElement('div');
 square.classList.add('square');
 row.classList.add('row');
 
-let userInput = 5;
+let userInput = 25;
 
 function addRows(userInput) {
     for (let i = 1; i <= (userInput * userInput); i++) {
         if (i % userInput == 0) {
             rowContainer.appendChild(row.cloneNode(true));
-        }
-    };
-}
+        };
+        };
+};
 
 function addSquares(userInput) {
     const rows = document.querySelectorAll('#row-container > *');
     rows.forEach(row => {
         for (let i = 1; i <= userInput; i++) {
             row.appendChild(square.cloneNode(true));
-        }
+        };
     });
-}
+};
 
 addRows(userInput)
 addSquares(userInput)
+
+document.querySelectorAll('.square').forEach(square => {
+    square.addEventListener('mouseenter', (e) => {
+        square.classList.add('square-hover');
+    });
+});
