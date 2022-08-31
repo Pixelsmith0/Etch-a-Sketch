c8ef34ad9e4f4cfa66de84b031684110f9b05382
M <mfetterly91@gmail.com>
2022-08-31 10:13:27 -0700

    JS - Added button to generate new div

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ const row = document.createElement('div');
 const square = document.createElement('div');
 square.classList.add('square');
 row.classList.add('row');
+let userInput = getUserInput();
+//Asks the user to input a number <=100, if they input a number >100, defaults to 100. Returns that value
+function getUserInput() {
+    let userInput = prompt('Please enter the number of rows you would like. (Max 100)', '');
+    if (userInput > 100) {
+        userInput = 100;
+    }
+    return userInput;
+};
 
-let userInput = 20;
-
+// Adds (userInput) amount of ".row" divs to #row-container
 function addRows(userInput) {
     for (let i = 1; i <= userInput; i++) {
             rowContainer.appendChild(row.cloneNode(true));
         };
     };
-    
+// Adds (userInput) amount of ".square(s)"" to each child of #row-container
 function addSquares(userInput) {
     const rows = document.querySelectorAll('#row-container > *');
     rows.forEach(row => {
@@ -20,7 +28,7 @@ function addSquares(userInput) {
         };
     });
 };
-
+// Adds hover-trail effect to all individual ".square(s)" 
 function addHoverEffect() {
     document.querySelectorAll('.square').forEach(square => {
         square.addEventListener('mouseenter', (e) => {
@@ -28,7 +36,24 @@ function addHoverEffect() {
         });
     });
 };
-
-addRows(userInput)
-addSquares(userInput)
+// Removes all ".row" divs
+function clearGrid() {
+    const toRemove = document.querySelectorAll('.row')
+    for (const row of toRemove) {
+        row.remove();
+    };  
+};
+// Adds the initial grid
+addRows(userInput);
+addSquares(userInput);
 addHoverEffect();
+
+// On click, removes all divs in #row-container, gets the desired grid size from the user, adds the appropriate amount of 
+// .row divs and .square divs, and adds the hover effect.
+document.querySelector('#generate-btn').addEventListener('click', (e) => {
+    clearGrid();
+    let newInput = getUserInput();
+    addRows(newInput);
+    addSquares(newInput);
+    addHoverEffect();
+});
